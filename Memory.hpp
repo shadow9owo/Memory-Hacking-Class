fa004c73e5483e29efb448b61bc5c3a28db5e3bd
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2026-02-13 16:03:25 +0100

    Update Memory.hpp

--- Memory.hpp
+++ Memory.hpp
@@ -1,25 +1,24 @@
 #pragma once
-#include "stdafx.hpp"
 #include "Memory.hpp"
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <string>
+#include <iostream>
 #include <psapi.h>
 #pragma comment(lib, "psapi")
-using std::string;
 
 class Memory
 {
 public:
-    int GetProcessId(char* processName);
-    int GetModuleBase(HANDLE processHandle, string &sModuleName);
+    int GetProcessId(const char* processName);
+    long GetModuleBase(HANDLE processHandle, std::string& sModuleName);
     BOOL SetPrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege);
     BOOL GetDebugPrivileges(void);
-    int ReadInt(HANDLE processHandle, int address);
-    int GetPointerAddress(HANDLE processHandle, int startAddress, int offsets[], int offsetCount);
-    int ReadPointerInt(HANDLE processHandle, int startAddress, int offsets[], int offsetCount);
-    float ReadFloat(HANDLE processHandle, int address);
-    float ReadPointerFloat(HANDLE processHandle, int startAddress, int offsets[], int offsetCount);
-    char* ReadText(HANDLE processHandle, int address);
-    char* ReadPointerText(HANDLE processHandle, int startAddress, int offsets[], int offsetCount);
+    int ReadInt(HANDLE processHandle, long address);
+    int GetPointerAddress(HANDLE processHandle, long startAddress, int offsets[], int offsetCount);
+    int ReadPointerInt(HANDLE processHandle, long startAddress, int offsets[], int offsetCount);
+    float ReadFloat(HANDLE processHandle, long address);
+    float ReadPointerFloat(HANDLE processHandle, long startAddress, int offsets[], int offsetCount);
+    char* ReadText(HANDLE processHandle, long address);
+    char* ReadPointerText(HANDLE processHandle, long startAddress, int offsets[], int offsetCount);
 };
